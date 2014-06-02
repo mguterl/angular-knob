77cd2d497f3fdb38392a9970c1bc951d4e2b053d
Michael Guterl <michael@diminishing.org>
2014-06-02 19:34:37 -0400

    Initial import

---
+++ README.md
@@ -0,0 +1,10 @@
+# Angular Knob
+
+**This is not a complete solution, if you are looking for something more
+complete check [https://github.com/yunlzheng/angular-knob]()**
+
+Evil Trout (Robin Ward) put together a really [great
+video](https://www.youtube.com/watch?v=S_l_DL8ysQQ&feature=youtu.be) on showing
+how to build a reusable Ember Component that wraps a jQuery plugin. I've been
+working with Angular lately and I wanted to go through the process of building
+the same component with Angular.
---
+++ angular-knob.js
@@ -0,0 +1,21 @@
+angular.module('angular-knob', []);
+angular.module('angular-knob').directive('knob', function() {
+  return {
+    restrict: 'E',
+    template: '<div class="knob"></div>',
+    link: function($scope, $element, $attrs) {
+      $element.knob({
+        fgColor: $attrs.fgColor,
+        change: function(value) {
+          $scope.$apply(function() {
+            $scope[$attrs.value] = value;
+          });
+        }
+      });
+
+      $scope.$watch($attrs.value, function(newValue, oldValue) {
+        $element.val(newValue).trigger('change');
+      });
+    }
+  }
+})
