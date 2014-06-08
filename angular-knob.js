813016c867ef96a65182d66a5404ee2dbeaa57d8
Chris Nelson <chris@gaslightsoftware.com>
2014-06-07 23:01:39 -0400

    Using isolate scope is more angularific

--- angular-knob.js
+++ angular-knob.js
@@ -1,18 +1,21 @@
 angular.module('angular-knob', []).directive('knob', function() {
   return {
     restrict: 'E',
+    scope: {
+      value: "="
+    },
     template: '<div class="knob"></div>',
     link: function($scope, $element, $attrs) {
       $element.knob({
         fgColor: $attrs.fgColor,
         change: function(value) {
           $scope.$apply(function() {
-            $scope[$attrs.value] = value;
+            $scope.value = value;
           });
         }
       });
 
-      $scope.$watch($attrs.value, function(newValue, oldValue) {
+      $scope.$watch("value", function(newValue, oldValue) {
         $element.val(newValue).trigger('change');
       });
     }
